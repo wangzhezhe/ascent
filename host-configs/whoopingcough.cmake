6ad36063c3a06b2ba1c69d204679a3b093752c00
Dave Pugmire <dpugmire@gmail.com>
2018-02-15 10:31:07 -0500

    Add in adios staging functionality.

--- host-configs/whoopingcough.cmake
+++ host-configs/whoopingcough.cmake
@@ -101,8 +101,9 @@ set(CONDUIT_DIR "/disk2TB/proj/ascent/conduit/conduit/install" CACHE PATH "")
 set(ASCENT_VTKM_USE_TBB OFF CACHE PATH "")
 #set(TBB_DIR "/usr/include" CACHE PATH "")
 
-# vtkm
-set(VTKM_DIR "/home/pugmire/proj/vtkm/kitware.master/install" CACHE PATH "")
+# vtkm/h
+set(VTKM_DIR "/disk2TB/proj/ascent/vtk-m/install" CACHE PATH "")
+set(VTKH_DIR "/disk2TB/proj/ascent/vtk-h/vtk-h/install" CACHE PATH "")
 
 # HDF5 support (optional)
 #
